cd48c7dacdd2c7d101c9ef15c157b05aff70aee2
Divyansh Sharma <divyanshsharma7765@gmail.com>
2021-06-01 00:01:38 +0530

    user login and sign up updated with JWT

--- controllers/globalErrorHndler.js
+++ controllers/globalErrorHndler.js
@@ -5,6 +5,11 @@ const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
+const handleJWTError = () =>
+  new AppError('Invalid token ,Please login again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired, please login again', 401);
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
@@ -65,6 +70,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
