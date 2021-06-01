6318fe9a997f8b288e67c2f1c0934939d25690af
divyansh_sharma_au16 <divyanshsharma7765@gmail.com>
2021-06-02 04:43:21 +0530

    user authorization authentication and user data modification implemented

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/globalErrorHndler');
@@ -11,22 +16,34 @@ const viewRouter = require('./routes/viewRoute');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
-//serving static files
 //global middlewares
-app.use(express.static(path.join(__dirname,'public')));
+//set security HTTP
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//limit request
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too Many requests from this IP ,please try again after an hour',
+});
+app.use('/api', limiter);
 
-app.use(express.json());
+//body parser
+app.use(express.json({ limit: '10kb' }));
 
+app.use(mongoSanitize());
 
+app.use(xss());
 
-
+app.use(hpp());
+//serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', viewRouter);
