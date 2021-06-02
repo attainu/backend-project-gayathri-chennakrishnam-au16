842dface8c2af98a1dfd18996068e2b79bb4223a
gayathri-chennakrishnam-au16 <sharmaanaga620@gmail.com>
2021-06-02 18:00:45 +0530

    changes in gardens template to populate data from database

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 
+
 app.use(express.json());
+
+
 
 
 
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,13 +1,32 @@
+const Garden = require('../models/gardenModel');
+const catchAsync = require('../utils/catchAync');
+
+
 exports.getOveriew = (req,res) => {
     res.status(200).render('overview',{
-      title: 'overview about us',
-      tab: 'OUR GALLERY'
+      title: 'overview about us'
     });
 };
 
-exports.getGardens = (req,res) => {
+
+exports.getGardens = catchAsync(async (req,res,next) => {
+
+  //get allGradens data from collection
+  const gardens = await Garden.find();
+  //build and render template using allGardens data from step 1
+
     res.status(200).render('gardens',{
       title: 'Visit our gardens',
-      tab: 'ABOUT US'
+      gardens
     });
+});
+
+//build template and render it.
+
+
+
+exports.getGarden = (req, res)=>{
+  res.status(200).render('garden',{
+    title: 'The Veggies Garden'
+  });
 };
